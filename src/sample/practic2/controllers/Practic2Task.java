3cf30132a09812b99628de8e5956bea7f9f28706
Vasyl Andrusiak <vasyl.andrusiak2017@gmail.com>
2019-10-18 15:14:21 +0200

    first commit

--- src/sample/practic2/controllers/Practic2Task.java
+++ src/sample/practic2/controllers/Practic2Task.java
@@ -1,12 +1,21 @@
 package sample.practic2.controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class Practic2Task {
+    private final double A = 1.62;
+    private final double E = 2.71;
+    private double x;
+    private double res;
     @FXML
     private ResourceBundle resources;
 
@@ -30,7 +39,31 @@ public class Practic2Task {
 
     @FXML
     void initialize() {
+        exit2Task.setOnAction(event -> {
+            exit2Task.getScene().getWindow().hide();
 
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/sample/main.fxml"));
 
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+        });
+        calculate2Task.setOnAction(event -> {
+            x = Double.parseDouble(arg2Task.getText());
+            if(x == A){
+                res = (Math.pow(E, Math.abs(x + A))) * Math.sin(x);
+            }else{
+                res = Math.pow(x - A, 2.0) * Math.pow(Math.cos(x), 2.0);
+            }
+            result2LTask.setText(Double.toString(res));
+        });
     }
 }
